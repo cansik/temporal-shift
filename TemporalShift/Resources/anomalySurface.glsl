bb2cf690b06bd9d70651997a27707513726884a6
Florian Bruggisser <florian@bildspur.ch>
2019-03-08 21:24:58 +0100

    added distance filter

--- TemporalShift/Resources/anomalySurface.glsl
+++ TemporalShift/Resources/anomalySurface.glsl
@@ -17,3 +17,8 @@ vec2 center = normalized_position.xy - vec2(0.5);
 float len = length(center);
 float vignette = smoothstep(RADIUS, RADIUS-SOFTNESS, len);
 _surface.diffuse = mix(vec4(0.0, 0.0, 0.0, 0.0), _surface.diffuse, vignette);
+
+// apply distance filter
+float dist = length(_surface.view - _surface.position);
+float nd = dist / 1.0;
+_surface.diffuse = mix(_surface.diffuse, vec4(0.0, 0.0, 0.0, 0.0), nd);
